10849f1ccfc2c34d88281a8699235923e102727e
pee272 <141197959+pee272@users.noreply.github.com>
2026-03-17 16:18:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,35 +4,15 @@ const path = require('path');
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
-
-const io = new Server(server, { 
-    cors: { origin: "*" } 
-});
-
-// Ye line sabse zaroori hai files dikhane ke liye
+const io = new Server(server, { cors: { origin: "*" } });
 app.use(express.static(__dirname));
-
 app.get('/', (req, res) => {
-    // Yahan humne file path sahi kar diya hai
     res.sendFile(path.join(__dirname, 'customer.html'));
 });
-
 io.on('connection', (socket) => {
-    socket.on('join_table', (tableId) => { 
-        socket.join(tableId); 
-        console.log("Table Joined: " + tableId);
-    });
-    
-    socket.on('mobile_scanned', (tableId) => { 
-        io.to(tableId).emit('mobile_connected', { table: tableId }); 
-    });
-
-    socket.on('new_order', (orderData) => { 
-        io.emit('display_order_to_manager', orderData); 
-    });
+    socket.on('join_table', (tableId) => { socket.join(tableId); console.log("Table Joined: " + tableId); });
+    socket.on('mobile_scanned', (tableId) => { io.to(tableId).emit('mobile_connected', { table: tableId }); });
+    socket.on('new_order', (orderData) => { io.emit('display_order_to_manager', orderData); });
 });
-
 const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => { 
-    console.log(`Server running on port ${PORT}`); 
-});
+server.listen(PORT, () => { console.log(`Server running on port ${PORT}`); });
