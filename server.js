99890c112de160dafe742d5bbf7b2c59fe723909
pee272 <141197959+pee272@users.noreply.github.com>
2026-03-17 16:41:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,14 +5,31 @@ const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server, { cors: { origin: "*" } });
+
 app.use(express.static(__dirname));
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'customer.html'));
 });
+
 io.on('connection', (socket) => {
-    socket.on('join_table', (tableId) => { socket.join(tableId); console.log("Table Joined: " + tableId); });
-    socket.on('mobile_scanned', (tableId) => { io.to(tableId).emit('mobile_connected', { table: tableId }); });
-    socket.on('new_order', (orderData) => { io.emit('display_order_to_manager', orderData); });
+    socket.on('join_table', (tableId) => { 
+        socket.join(tableId); 
+    });
+
+    socket.on('mobile_scanned', (tableId) => { 
+        io.to(tableId).emit('mobile_connected', { table: tableId }); 
+    });
+
+    // Mobile se Laptop/Tablet sync karne ke liye
+    socket.on('sync_cart', (data) => {
+        io.to(data.table).emit('update_client_view', data);
+    });
+
+    socket.on('new_order', (orderData) => { 
+        io.emit('display_order_to_manager', orderData); 
+    });
 });
+
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => { console.log(`Server running on port ${PORT}`); });
